488330a5796fe83c616d9459fdb34ce668e3e954
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 13:06:52 -0400

    include demo of cookie hashing script

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import hashlib
 
+
 def has_str(s):
-	return hashlib.md5(s).hexdigest()
+    return hashlib.md5(s).hexdigest()
+
 
 def make_secure_val(s):
-	return "%s|%s" % (s, has_str(s))
+    return "%s|%s" % (s, has_str(s))
+
 
 def check_secure_val(h):
-	val = h.split('|')[0]
-	if h == make_secure_val(val):
-		return val
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
+
 
 def gae_cookie_hasher():
-	visits = 0
-	visit_cookie_str = self.request.cookies.get('visits')
+    visits = 0
+    visit_cookie_str = self.request.cookies.get('visits')
 
-	if visit_cookie_str:
-		cookie_val = check_secure_val(visit_cookie_str)
-		if cookie_val:
-			visits = int(cookie_val)
+    if visit_cookie_str:
+        cookie_val = check_secure_val(visit_cookie_str)
+        if cookie_val:
+            visits = int(cookie_val)
 
-	visits += 1
+    visits += 1
 
-	new_cookie_val = make_secure_val(str(visits))
+    new_cookie_val = make_secure_val(str(visits))
 
-	self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+    self.response.headers.add_header(
+        'Set-Cookie', 'visits=%s' % new_cookie_val)
