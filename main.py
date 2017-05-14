9afe3cdda8f0cd9b3b42ab898029c6f0833c658c
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 13:06:28 -0400

    include demo of cookie hashing script

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ def check_secure_val(h):
 	val = h.split('|')[0]
 	if h == make_secure_val(val):
 		return val
+
+def gae_cookie_hasher():
+	visits = 0
+	visit_cookie_str = self.request.cookies.get('visits')
+
+	if visit_cookie_str:
+		cookie_val = check_secure_val(visit_cookie_str)
+		if cookie_val:
+			visits = int(cookie_val)
+
+	visits += 1
+
+	new_cookie_val = make_secure_val(str(visits))
+
+	self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
