d35543072ac559d75328fa09caa0080fd6662dd4
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 14:31:19 -0400

    add password hashing with salt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import hmac
+import random
+import string
+import hashlib
 
 SECRET = "imsosecret"
 def has_str(s):
@@ -14,6 +17,13 @@ def check_secure_val(h):
     if h == make_secure_val(val):
         return val
 
+def make_salt():
+	return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw):
+	salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (h, salt)
 
 def gae_cookie_hasher():
     visits = 0
