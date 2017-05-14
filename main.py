0a91167a6fa3224e59dabfddc487d1f13caa36ec
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 13:03:08 -0400

    update to avoid bug gae with comma

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def has_str(s):
 	return hashlib.md5(s).hexdigest()
 
 def make_secure_val(s):
-	return "%s,%s" % (s, has_str(s))
+	return "%s|%s" % (s, has_str(s))
 
 def check_secure_val(h):
-	val = h.split(',')[0]
+	val = h.split('|')[0]
 	if h == make_secure_val(val):
 		return val
